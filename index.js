1589a35c37c350655fea6d09847997882b359454
Rishu Anand <rishuanandchaudhary@yahoo.com>
2018-07-21 16:50:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,16 +2,19 @@
 var kittens = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendKitten (name) {
+  // Alters original kittens
   kittens.push(name);
   return(kittens);
 }
 
 function destructivelyPrependKitten (name) {
+  // Alters original kittens
   kittens.unshift(name);
   return(kittens);
 }
 
 function destructivelyRemoveLastKitten () {
+  // Alters original kittens
   kittens.pop();
   return(kittens)
 }
